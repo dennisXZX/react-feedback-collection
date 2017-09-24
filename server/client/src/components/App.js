98be9be751e6565b6322b2a9bf4a9d72efe4da55
Dennis Xiao <dennisboys@gmail.com>
2017-09-24 17:27:02 +1000

    added routes

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
 	return (
 		<div>
-			<div>
-				Hi There!
-			</div>
-			<a href="/auth/google">Sign in with Google</a>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route path="/" exact={true} component={Landing} />
+					<Route path="/surveys" exact={true} component={Dashboard} />
+					<Route path="/surveys/new" exact={true} component={SurveyNew} />
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,14 +1,18 @@
+// React stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// Redux stuff
 import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+// React-redux stuff
+import { Provider } from 'react-redux';
 
 import App from './components/App';
-import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
+	// Provider is a component that makes the store accessible to every component in the app
 	<Provider store={store}>
 		<App />
 	</Provider>,
