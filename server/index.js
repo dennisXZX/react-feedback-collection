02c6dbe76ad3f3e48c6c003825440f5fdf17445b
Dennis Xiao <dennisboys@gmail.com>
2017-09-25 14:57:59 +1000

    added heroku production support

--- server/index.js
+++ server/index.js
@@ -41,6 +41,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html if it doesn't recognize a route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // use Heroku port in production environment or port 5000 in local development environment
 const PORT = process.env.PORT || 5000;
 
