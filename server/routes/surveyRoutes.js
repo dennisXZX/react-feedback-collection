de5a9ea57f9dc9013cc7ac8e8ffe58b698ce1b57
Dennis Xiao <dennisboys@gmail.com>
2017-09-29 22:09:13 +1000

    added webhook

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+// nodeJS library for parsing URL
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredit = require('../middlewares/requireCredit');
@@ -13,9 +17,53 @@ module.exports = (app) => {
 	});
 
 	app.post('/api/surveys/webhooks', (req, res) => {
-		console.log(req.body);
+
+		const p = new Path('/api/surveys/:surveyId/:choice');
+
+		// map over the event notification object from SendGrid to extract the info we need
+		const events = _.map(req.body, (event) => {
+			// extract the pathname from the URL
+			const pathname = new URL(event.url).pathname;
+
+			// extract from the path an object {surveyId: simpleSurveyId, choice: 'yes'}
+			// return null if the path does not match
+			const match = p.test(pathname);
+
+			if (match) {
+				return {
+					email: event.email,
+					surveyId: match.surveyId,
+					choice: match.choice
+				};
+			}
+		});
+
+		// remove all 'undefined' elements
+		const compactEvents = _.compact(events);
+		// remove all duplicated elements
+		const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+		// iterate uniqueEvents array and run a MongoDB query on each event
+		uniqueEvents.forEach((event) => {
+			// find a survey with the specified surveyId
+			// and a sub-document collection record with the specified email and responded values
+			Survey.updateOne({
+				_id: event.surveyId,
+				recipients: {
+					$elemMatch: { email: event.email, responded: false }
+				}
+			}, {
+				// increment property 'yes' or 'no' by one, using ES6 key interpolation
+				$inc: { [event.choice]: 1 },
+				// set the 'responded' property of recipients sub-document collection record found by ($elemMatch) to 'true'
+				$set: { 'recipients.$.responded': true }
+			}).exec(); // doesn't handle the promise as SendGrid doesn't expect a response.
+		});
+
+		// send an empty object so the request is not pending
 		res.send({});
-	});
+
+	}); // END of post('/api/surveys/webhooks')
 
 	app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
@@ -52,6 +100,6 @@ module.exports = (app) => {
 			res.status(422).send(err);
 		}
 
-	}); // END of app.post('/api/surveys')
+	}); // END of post('/api/surveys')
 
 };
